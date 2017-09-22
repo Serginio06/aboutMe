d4729518ee05f3714d988582f26a782c6fec2ad2
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-09-22 14:18:46 +0300

    aboutMe. Preparation for git.page deploy v0.0.2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <p>
+              <a href="https://serginio06.github.io/angular-ivanchenko/#!/">My first project</a>
+          </p>
       </div>
     );
   }
